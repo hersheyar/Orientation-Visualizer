63e4fde3d290b31ff61e8665eda63cea260e9a93
Andrew H <hersheyar@outlook.com>
2025-11-06 19:36:58 -0600

    first

---
+++ OrientationVisualizer/OrientationVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrientationVisualizerApp.swift
+//  OrientationVisualizer
+//
+//  Created by Andrew Hershey on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct OrientationVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
